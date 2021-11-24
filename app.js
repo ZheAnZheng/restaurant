35f0701c11ce4df0250f4fd433ed8c2fed2fb7f1
nixon <a37007647@gmail.com>
2021-11-24 15:59:20 +0000

    add style and add cdn

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express=require('express');
 const app=express();
 const {engine}=require('express-handlebars');
 
+
+//setting handlebars
 app.engine('handlebars',engine({defaultLayout:'main'}));
 app.set('view engine','handlebars');
 app.set('views','./views');
 
+// make 'public' directory static
+app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
     res.render('index');
+})
+
+app.get('/show',(req,res)=>{
+    res.render('show');
 })
 
 app.listen(3000,()=>{
