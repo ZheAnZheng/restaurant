f842ca82dfba4765f4c7e24bced32e5449a9c4b1
nixon <a37007647@gmail.com>
2021-11-24 16:33:24 +0000

    complete restaurant page

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express=require('express');
-const app=express();
-const {engine}=require('express-handlebars');
-
+const express = require("express");
+const app = express();
+const { engine } = require("express-handlebars");
+const restaurants = require("./public/restaurant.json").results;
 
 //setting handlebars
-app.engine('handlebars',engine({defaultLayout:'main'}));
-app.set('view engine','handlebars');
-app.set('views','./views');
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 // make 'public' directory static
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index", { restaurants });
+});
 
-app.get('/show',(req,res)=>{
-    res.render('show');
-})
+app.get("/show", (req, res) => {
+  res.render("show");
+});
 
-app.listen(3000,()=>{
-    console.log('server run on http://localhost:3000');
-})
+app.listen(3000, () => {
+  console.log("server run on http://localhost:3000");
+});
